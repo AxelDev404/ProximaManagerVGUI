357d407d509f80d8dd9883e2e7e865a8f7eed40c
Alexandru Stefan Apetroae <99222817+AxelDev404@users.noreply.github.com>
2024-10-17 16:43:18 +0200

    Complete SingUp Panell
    The sing up pannel is complete

--- app/gui/register.py
+++ app/gui/register.py
@@ -22,10 +22,11 @@ class Register:
 
         customFontForSingUp = tkFont.Font(family="Tech Noir" , size=50)
         customFontSlogan = tkFont.Font(family="Tech Noir" , size=35)
+        customFontRegister = tkFont.Font(family = "Tech Noir" , size=13)
         
         self.root = root
         #self.root.attributes("-fullscreen" , True)
-        self.size = tk.Tk.geometry(self.root,"1200x700+350+200")
+        self.size = tk.Tk.geometry(self.root,"1200x700+660+300")
         self.resizable = tk.Tk.resizable(self.root, False , False)
         self.root.attributes('-topmost' , 0) 
         self.root.iconbitmap(icon_path)
@@ -63,14 +64,47 @@ class Register:
         self.Slogan5MainFrame2.place(x=310 , y=150)
 
 
-        img1 = r"C:/Users/alexa/Desktop/WorkSpace/WorkSpace/Python_projects/ProximaManagerVGUI/assets/img/cyber.png"   
-        self.imageOpen1 = Image.open(img1).resize((300,300))
+        img1 = r"C:/Users/alexa/Desktop/WorkSpace/WorkSpace/Python_projects/ProximaManagerVGUI/assets/img/account-protection.png"   
+        self.imageOpen1 = Image.open(img1).resize((500,500))
         self.imageSecurty = ImageTk.PhotoImage(self.imageOpen1)
 
         self.lockSec = tk.Label(self.MainFrame2 , image=self.imageSecurty , bg="#0D112B")
-        self.lockSec.place(x=100 , y=270)
+        self.lockSec.place(x=50 , y=270)
         self.lockSec.image = self.imageSecurty #self.lockSec.image per fare riferimento all ogetto dell immagine quando ci troviamo in altre pagine di routing
 
+
+        self.usernameText = ttk.Label(self.MainFrame , text="Username" , font="Helvetica 11 " , foreground="#0787FF" , background="#070A1C")
+        self.usernameText.place(x=140, y=170)
+        self.usernameSingUp = tk.Entry(self.MainFrame , width=41 , font=("Arial" , 11) , takefocus=0 , borderwidth=1 ,bg="white" , foreground="black")
+        self.usernameSingUp.place(x=140, y=200 , height=35)
+
+
+        self.passwordText = ttk.Label(self.MainFrame , text="Password" , font="Helvetica 11 " , foreground="#0787FF" , background="#070A1C")
+        self.passwordText.place(x=140, y=250)
+        self.passwordInput = tk.Entry(self.MainFrame ,  width=41 , font=("Arial" , 11) , takefocus=0 , borderwidth=1 ,bg="white" , foreground="black")
+        self.passwordInput.place(x=140, y=280 , height=35)
+
+
+        self.emailText = ttk.Label(self.MainFrame , text="Email" , font="Helvetica 11 " , foreground="#0787FF" , background="#070A1C")
+        self.emailText.place(x=140, y=330)
+        self.emailInput = tk.Entry(self.MainFrame ,  width=41 , font=("Arial" , 11) , takefocus=0 , borderwidth=1 ,bg="white" , foreground="black")
+        self.emailInput.place(x=140, y=360 , height=35)
+
+
+        self.nameText = ttk.Label(self.MainFrame , text="Name" , font="Helvetica 11 " , foreground="#0787FF" , background="#070A1C")
+        self.nameText.place(x=140, y=410)
+        self.nameTextInput = tk.Entry(self.MainFrame ,  width=41 , font=("Arial" , 11) , takefocus=0 , borderwidth=1 ,bg="white" , foreground="black")
+        self.nameTextInput.place(x=140, y=440 , height=35)
+
+
+        self.phoneText = ttk.Label(self.MainFrame , text="Phone Number" , font="Helvetica 11 " , foreground="#0787FF" , background="#070A1C")
+        self.phoneText.place(x=140, y=490)
+        self.phoneTextInput = tk.Entry(self.MainFrame ,  width=41 , font=("Arial" , 11) , takefocus=0 , borderwidth=1 ,bg="white" , foreground="black")
+        self.phoneTextInput.place(x=140, y=520 , height=35)
+
+
+        self.buttonRegister = tk.Button(self.MainFrame , text="Sing Up" , bg="#0787FF" , takefocus=0 , width=25 , height=3 , foreground="white" , font=customFontRegister , borderwidth=0)
+        self.buttonRegister.place(x=140 , y=590)
 
         
 
