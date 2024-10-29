e950c2456cc5ee5b47e87ce10e1a3315a79dc753
Alexandru Stefan Apetroae <99222817+AxelDev404@users.noreply.github.com>
2024-10-29 13:05:35 +0100

    Removed resizable
    The page will not be resizable , added the add credential pannel

--- app/gui/dashboard.py
+++ app/gui/dashboard.py
@@ -29,11 +29,15 @@ class DashBoard:
         self.root = root
         self.root  = root #test private after
         self.root.title("PROXIMA MANAGER")
-        self.size = tk.Tk.geometry(self.root , "1700x800+350+200")
-        #self.resizable = tk.Tk.resizable(self.root, False , False)
+        self.size = tk.Tk.geometry(self.root , "1300x700+350+200")
+        self.resizable = tk.Tk.resizable(self.root, False , False)
         self.root.attributes('-topmost' , 0) 
         self.root.iconbitmap(icon_path)
         #self.root.configure(background = "#121528")
+
+        self.root.grid_columnconfigure(1, weight=1)  # Colonna 1 (contenuto) si espande
+        self.root.grid_rowconfigure(0, weight=1)     # Riga 0 si espande verticalmente
+
 
         style = ttk.Style()
         style.theme_use("clam")
@@ -42,30 +46,94 @@ class DashBoard:
 
         
         self.MenuFrame = ttk.Frame(self.root , style="MenuFarme.TFrame" , width=300 , height=700)
-        self.MenuFrame.pack(fill="y" , side=LEFT)
-        self.MenuFrame.pack_propagate(False) # blocco estensione del menu
+        self.MenuFrame.grid(row=0, column=0, sticky="ns")
+        self.MenuFrame.grid_propagate(False)       
+        #self.MenuFrame.pack(fill="y" , side=LEFT)
+        #self.MenuFrame.pack_propagate(False) # blocco estensione del menu
 
         self.MainFrame = ttk.Frame(self.root , style="MainFrame.TFrame" , width=1500 , height=700)
-        self.MainFrame.pack(fill=BOTH , expand=True , side=LEFT)
+        #self.MainFrame.pack(fill=BOTH , expand=True , side=LEFT)
+        self.MainFrame.grid(row=0, column=1, sticky="nsew")
+        
+        self.MainFrame.grid_columnconfigure(0, weight=1)
+        self.MainFrame.grid_rowconfigure(0, weight=1)
 
-        self.tab1 = tk.Frame(self.MainFrame , width=1500 , background = "#121528")
-        self.tab2 = tk.Frame(self.MainFrame , width=1500 , background = "#121528")
-        self.tab3 = tk.Frame(self.MainFrame , width=1500 , background = "#121528")
-        self.tab4 = tk.Frame(self.MainFrame , width=1500 , background = "#121528")
-        self.tab5 = tk.Frame(self.MainFrame , width=1500 , background = "#121528")
 
-        self.labelTab1 = tk.Label(self.tab1, text="TEST TAB 1")
+        self.tab1 = tk.Frame(self.MainFrame , width=1500 , bg = "#121528")
+        #self.tab1.pack(fill=BOTH , expand=True)
+
+
+        #self.tab1.grid_columnconfigure(0, weight=1)
+        #self.tab1.grid_rowconfigure(0, weight=1)
+
+        self.tab2 = tk.Frame(self.MainFrame , width=1500 , bg = "#121528")
+        self.tab3 = tk.Frame(self.MainFrame , width=1500 , bg = "#121528")
+        self.tab4 = tk.Frame(self.MainFrame , width=1500 , bg = "#121528")
+        self.tab5 = tk.Frame(self.MainFrame , width=1500 , bg = "#121528")
+
+        for tab in [self.tab1, self.tab2, self.tab3, self.tab4, self.tab5]:
+            tab.grid_rowconfigure(0, weight=1)  # Permette l'espansione della colonna
+            tab.grid_rowconfigure(1, weight=1) #tolgo l espansione con lo 0   
+            tab.grid_rowconfigure(2, weight=1) 
+            tab.grid_rowconfigure(3, weight=1) #tolgo l espansione con lo 0   
+            tab.grid_rowconfigure(4, weight=1) 
+            
+            tab.grid_columnconfigure(0, weight=1)  # Permette l'espansione della colonna
+            tab.grid_columnconfigure(1, weight=1)   
+            tab.grid_columnconfigure(2, weight=1)  
+            tab.grid_columnconfigure(3, weight=1)   
+            tab.grid_columnconfigure(4, weight=1)  
+
+        
+
+        self.labelTab1 = tk.Label(self.tab1, text="TEST TAB 2")
         self.labelTab2 = tk.Label(self.tab2, text="TEST TAB 2")
         self.labelTab3 = tk.Label(self.tab3, text="TEST TAB 3")
         self.labelTab4 = tk.Label(self.tab4, text="TEST TAB 4")
         self.labelTab5 = tk.Label(self.tab5, text="TEST TAB 5")
         
 
-        self.labelTab1.pack()
-        self.labelTab2.pack()
-        self.labelTab3.pack()
-        self.labelTab4.pack()
-        self.labelTab5.pack()
+        #self.labelTab2.grid(row=0, column=0, sticky="nsew")  # Centro
+        #self.labelTab3.grid(row=0, column=0, sticky="nsew")  # Centro
+        #self.labelTab4.grid(row=0, column=0, sticky="nsew")  # Centro
+        #self.labelTab5.grid(row=0, column=0, sticky="nsew")  # Centro
+
+        #ADD CREDENTIAL TAB#
+
+        self.Title =tk.Label(self.tab1 , text="Add Credentials" , font="Inter 13" , foreground="white" , background="#121528")
+        self.Title.grid(row = 0 , column = 2 , sticky="n" , pady=20)
+
+        self.usernameTitle = tk.Label(self.tab1 , text="Username" , font="Inter 10" ,  foreground="white" , background="#121528")
+        self.usernameTitle.grid(row=1 , column=1 , sticky="sw" , pady=0, padx=(100,0))
+
+        #self.showTab(self.tab1)
+        self.usernameRegistration = tk.Entry(self.tab1 , width=35 , border=1 , bg="white" , foreground="black" , font=("Inter" , 11))
+        self.usernameRegistration.grid(row=2 , column=1 , sticky="nw" , padx=(100,0) , pady=(0,60))
+
+        self.passwordTitle =  tk.Label(self.tab1 , text="Password" , font="Inter 10" , foreground="white" , background="#121528")
+        self.passwordTitle.grid(row=2 , column=1 , sticky="sw" , padx=(100,0) )
+
+        self.passwordRegistration = tk.Entry(self.tab1 , width=35 , font=("Inter",11) , border=1 , bg="white" , foreground="black")
+        self.passwordRegistration.grid(row=3 , column=1 , sticky="nw" ,padx=(100,0) , pady=(0,400))
+
+        self.emailTitle = tk.Label(self.tab1 ,  text="Email" , font="Inter 10" , foreground="white" , background="#121528")
+        self.emailTitle.grid(row=2 , column=3 , sticky="sw")
+
+        self.emailRegistration = tk.Entry(self.tab1 , width=35 , font=("Inter",11) , border=1 , bg="white" , foreground="black")
+        self.emailRegistration.grid(row = 3 , column=3 , sticky="nw" , padx=(0,50))
+
+        self.prductTitle = tk.Label(self.tab1 ,  text="Service" , font="Inter 10" , foreground="white" , background="#121528")
+        self.prductTitle.grid(row = 1 , column=3 , sticky="sw")
+
+        self.productRegistration = tk.Entry(self.tab1 , width=35 , font=("Inter",11) , border=1 , bg="white" , foreground="black")
+        self.productRegistration.grid(row = 2 , column=3 , sticky="nw")
+
+
+
+
+
+
+
 
 
         self.Title = tk.Label(self.MenuFrame , text="Proxima Manger" , font=customFontSingIn , background="#1B1A36" , foreground="white")
@@ -121,6 +189,9 @@ class DashBoard:
 
         self.buttonSettings = tk.Button(self.AreaPersonal , image=self.labelSettings , background="#111735" , border=0 , padx=5 , pady=5 , activebackground="#111735" ,  cursor="hand2" , command=lambda:self.showTab(self.tab5))
         self.buttonSettings.pack(side="right" , padx=13 , pady=10)
+        
+        self.showTab(self.tab1)
+
 
     
     def logOut(self):
@@ -133,9 +204,9 @@ class DashBoard:
 
     def showTab(self, tab_frame):
         for Widget in self.MainFrame.winfo_children():
-            Widget.pack_forget()
+            Widget.grid_forget()
         
-        tab_frame.pack(fill = "both" , expand = True)
+        tab_frame.grid(sticky="nsew")
     
 
     def on_hover(self, e):
